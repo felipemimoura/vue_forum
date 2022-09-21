e6a0b4b4bdd5931ea50c281c5074d46d214db28f
Felipe Moura <felipemimoura@gmail.com>
2022-09-20 22:39:33 -0300

    feat: create a page home and find posts by id e users

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,10 +1,35 @@
 <template>
   <div>Hello form page home</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{thread.title}}</h2>
+
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{userById(postById(postId).userId).name}}</p>
+      <p>{{postById(postId).text}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import sourceData from '@/data.json';
 
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
+  },
+  methods: {
+    postById(postId) {
+      return this.posts.find((post) => post.id === postId);
+    },
+
+    userById(userId) {
+      return this.users.find((post) => post.id === userId);
+    },
+  },
 };
 </script>
 
