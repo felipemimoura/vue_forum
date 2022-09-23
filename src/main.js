c7ca6f0c03180d0a8550ccba7118d2e0295e1319
Felipe Moura <felipemimoura@gmail.com>
2022-09-23 16:17:27 -0300

    feat: create a new router with props

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue';
 import HomePage from '@/components/PageHome.vue';
+import PageThreadShow from '@/components/PageThreadShow.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
+  {
+    path: '/thread/:id', name: 'Thread', component: PageThreadShow, props: true,
+  },
 ];
 
 const router = createRouter({
