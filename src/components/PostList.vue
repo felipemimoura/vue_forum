0dd5060e1a4d9e9ac1f4faa413b8c2e7e1e017c9
Felipe Moura <felipemimoura@gmail.com>
2022-09-24 16:53:10 -0300

    feat: create post List component

---
+++ src/components/PostList.vue
@@ -0,0 +1,41 @@
+<template lang="">
+  <div>
+    <div class="post-list">
+      <div class="post" v-for="post in props.posts" :key="post.id">
+        <div class="user-info">
+          <a href="#" class="user-name">{{ userById(post.userId).name }}</a>
+
+          <a href="#">
+            <img class="avatar-large" :src="userById(post.userId).avatar" alt="teste" />
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              {{ post.text }}
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{ post.publishedAt }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import sourceData from '@/data.json'
+import { defineProps } from 'vue'
+
+const props = defineProps(['posts'])
+const { users } = sourceData
+
+// const postById = (postId) => posts.find((post) => post.id === postId)
+
+const userById = (userId) => users.find((post) => post.id === userId)
+</script>
+<style lang=""></style>
