fac8f3d03cf64620316cb017e127fc6151b7ffcb
Felipe Moura <felipemimoura@gmail.com>
2022-09-24 16:52:50 -0300

    feat: filter posts per thread

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -1,30 +1,7 @@
 <template>
-  <h1>{{ thread.title }}</h1>
-
-  <div class="post-list">
-    <div class="post" v-for="postId in thread.posts" :key="postId">
-      <div class="user-info">
-        <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
-
-        <a href="#">
-          <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="teste" />
-        </a>
-
-        <p class="desktop-only text-small">107 posts</p>
-      </div>
-
-      <div class="post-content">
-        <div>
-          <p>
-            {{ postById(postId).text }}
-          </p>
-        </div>
-      </div>
-
-      <div class="post-date text-faded">
-        {{ postById(postId).publishedAt }}
-      </div>
-    </div>
+  <div class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+    <PostList :posts="threadPosts" />
   </div>
 </template>
 
@@ -32,14 +9,14 @@
 import sourceData from '@/data.json'
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+import PostList from '@/components/PostList.vue'
 
 const route = useRoute()
 
-const { threads, posts, users } = sourceData
+const { threads, posts } = sourceData
 
 const thread = computed(() => threads.find((t) => t.id === route.params.id))
-const postById = (postId) => posts.find((post) => post.id === postId)
-const userById = (userId) => users.find((post) => post.id === userId)
+const threadPosts = computed(() => posts.filter((post) => post.threadId === route.params.id))
 </script>
 
 <style></style>
