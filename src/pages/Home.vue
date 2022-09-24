602bcdafd5b2d80d951994d0a9fffe888977a31a
Felipe Moura <felipemimoura@gmail.com>
2022-09-24 16:52:23 -0300

    refactor: remove comments lines

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,10 +7,6 @@ import sourceData from '@/data.json'
 import ThreadList from '@/components/ThreadList.vue'
 
 const { threads } = sourceData
-
-// const postById = (postId) => posts.find((post) => post.id === postId)
-
-// const userById = (userId) => users.find((post) => post.id === userId)
 </script>
 
 <style></style>
