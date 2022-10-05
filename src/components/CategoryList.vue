d499ea98982693f279ad352e230706cd77259d1c
Felipe Moura <felipemimoura@gmail.com>
2022-10-04 21:42:42 -0300

    feat: create category List components

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ForumList
+    v-for="category in props.categories"
+    :key="category.id"
+    :forums="getForumByCategory(category)"
+    :title="category.name"
+    :categoryId="category.id"
+  />
+</template>
+<script setup>
+import ForumList from './ForumList.vue'
+import sourceData from '@/data.json'
+import { reactive } from 'vue'
+const props = defineProps({
+  categories: {
+    required: true,
+    type: Array,
+  },
+})
+
+const state = reactive({
+  forums: sourceData.forums,
+})
+
+const getForumByCategory = (category) => {
+  return state.forums.filter((forum) => forum.categoryId === category.id)
+}
+</script>
