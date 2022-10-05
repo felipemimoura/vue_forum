cb4291f35ef2971b0f98f3e783d7266931c9df91
Felipe Moura <felipemimoura@gmail.com>
2022-10-04 21:43:20 -0300

    feat: create route to category page

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,18 @@ import HomePage from '@/pages/Home.vue';
 import ThreadShow from '@/pages/ThreadShow.vue';
 import NotFound from '@/pages/NotFound.vue'
 import Forum from '@/pages/Forum.vue'
+import Category from '@/pages/Category.vue'
 import { createRouter, createWebHistory } from 'vue-router';
 import sourceData from '@/data.json'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
+  {
+    path: '/category/:id',
+    name: 'Category',
+    component: Category,
+    props: true
+  },
   {
     path: '/thread/:id',
     name: 'Thread',
