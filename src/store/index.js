235a0716b801b9e76d479a33138f858d02b175a8
Felipe Moura <felipemimoura@gmail.com>
2022-10-06 19:22:25 -0300

    feat: create actiton and muttations to updated posts

--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,22 @@ import sourceData from '@/data.json'
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: sourceData
+  state: sourceData,
+
+  actions: {
+    createPost(context, post) {
+      post.id === `ggqq${Math.random()}`
+      context.commit('setPost', { post })
+      context.commit('appendPostToThread', { postId: post.id, threadId: post.threadId })
+    }
+  },
+  mutations: {
+    setPost(state, { post }) {
+      state.posts.push(post)
+    },
+    appendPostToThread(state, { postId, threadId }) {
+      const thread = state.threads.find(thread => thread.id === threadId)
+      thread.posts.push(postId)
+    }
+  }
 })
